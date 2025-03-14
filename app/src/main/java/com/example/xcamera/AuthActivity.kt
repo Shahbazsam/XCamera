0e123b087ef036becbffd7f5ae85717867b96137
Mohammad Shahbaz <mdshahbaz7896@gmail.com>
2025-03-15 01:42:51 +0700

    Minor Changes

--- app/src/main/java/com/example/xcamera/AuthActivity.kt
+++ app/src/main/java/com/example/xcamera/AuthActivity.kt
@@ -16,7 +16,7 @@ class AuthActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide()
+
         enableEdgeToEdge()
         setContent {
             XCameraTheme {
